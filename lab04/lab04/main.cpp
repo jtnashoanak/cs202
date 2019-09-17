640d3cb96c570e737f4b6af3795218062ecca767
jtnashoanak <jtnashoanak@alaska.edu>
2019-09-17 12:01:47 -0800

    Taken from lab before i set up Github

--- lab04/lab04/main.cpp
+++ lab04/lab04/main.cpp
@@ -1,1 +1,31 @@
+//  main.cpp
+//  lab04
+//  Created by Jadon Nashoanak on 9/17/19.
+
+#include <iostream>
+using std::cout;
+#include <fstream>
+using std::ofstream;
+using std::ifstream;
+using std::fstream;
+
+int main() {
+    
+    ifstream ifile("out.txt");
+    
+    while(true) {
+        auto x = 0;
+        ifile >> x;
+        
+        if(!ifile) {
+            if(ifile.eof()){
+                break;
+            }
+        }
+        cout<< "x is " << x << "\n";
+    }
+    
+    
+    return 0;
+}
 
