fa809563b3708f45e79dd70a1d1e94662ca33fbc
jtnashoanak <jtnashoanak@alaska.edu>
2019-09-26 16:28:27 -0800

    Creation of print(stream & ) member function that draws the box to the ostream

--- hw01/hw01/box.cpp
+++ hw01/hw01/box.cpp
@@ -3,6 +3,23 @@
 //  Created by Jadon Nashoanak.
 
 #include "box.hpp"
+#include <iostream>
+using std::ostream;
+using std::endl;
 
+ostream & Box::print(ostream & os) const {
+    
+    if(Box::type() == "Filled") {
+        
+        for(auto x = 0; x < Box::getHeight; x++) {
+            
+            for(auto y = 0; y < Box::getWidth; y++) {
+                
+                os << "X";
+            }
+            os << endl;
+        }
+    }
+}
 
 
