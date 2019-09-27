ac0576ffc253e9a50a383a73dcd7d7cc54c00d1f
jtnashoanak <jtnashoanak@alaska.edu>
2019-09-26 16:49:50 -0800

    Creation of Constructors

--- hw01/hw01/box.cpp
+++ hw01/hw01/box.cpp
@@ -50,4 +50,57 @@ ostream & Box::print(ostream & os) const {
 return os;
 }
 
+Box::Box() : _width(1), _height(1), _type(true){}
+Box::Box(int width, int height) : _type(true) {
+    
+    _width = width;
+    _height = height;
+}
+
+Box::Box(int width, int height, bool type) {
+    
+    _width = width;
+    _height = height;
+    _type = type;
+}
+
+Box::~Box(){}
+
+int Box::getHeight() const {
+    
+    return _height;
+}
+
+int Box::getWidth() const {
+    
+    return _width;
+}
+
+int Box::setHeight(int height) {
+    
+    _height = height;
+    return _height;
+}
+
+int Box::setWidth(int width) {
+    _width = width;
+    return _width;
+}
+
+bool Box::setType(bool type) {
+    
+    _type = type;
+    return _type;
+}
+
+string Box::type() const {
+    
+    if(Box::_type == false) {
+        
+        return "Hollow";
+    } else {
+        
+        return "Filled";
+    }
+}
 
