a97bbacbd349867a3c45897415169fae691535f7
jtnashoanak <jtnashoanak@alaska.edu>
2019-09-26 16:40:52 -0800

    Creation of empty box and newline

--- hw01/hw01/box.cpp
+++ hw01/hw01/box.cpp
@@ -11,15 +11,43 @@ ostream & Box::print(ostream & os) const {
     
     if(Box::type() == "Filled") {
         
-        for(auto x = 0; x < Box::getHeight; x++) {
+        for(auto x = 0; x < Box::getHeight(); x++) {
             
-            for(auto y = 0; y < Box::getWidth; y++) {
+            for(auto y = 0; y < Box::getWidth(); y++) {
                 
                 os << "X";
             }
             os << endl;
         }
+    } else {
+    
+        for(auto x = 0; x < Box::getHeight(); x++) {
+            
+            if(x == 0 || x == (Box::getHeight() - 1 )) {
+                
+                for(auto y = 0; y < Box::getWidth(); y++) {
+                    os << "X";
+                }
+                os << endl;
+                
+            } else {
+                  
+                for(auto x = 0; Box::getWidth(); x++) {
+                    
+                    if(x == 0 || x == (Box::getWidth() - 1)) {
+                    
+                    os << "X";
+                    } else {
+                    
+                    os << " ";
+                    }
+                    
+                os << endl;
+                }
+            }
+        }
     }
+return os;
 }
 
 
