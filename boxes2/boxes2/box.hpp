9cb05654f5af73b88360544af68a7725868d05b8
jtnashoanak <jtnashoanak@alaska.edu>
2019-10-18 12:14:15 -0800

    addition of new constructors for new box_test

--- boxes2/boxes2/box.hpp
+++ boxes2/boxes2/box.hpp
@@ -6,30 +6,33 @@
 #ifndef box_h
 #define box_h
 
-#include <iostream>
-using std::ostream;
+#include <ostream>
 #include <string>
 using std::string;
 
 class Box {
 
 public:
+    enum BoxType {FILLED, HOLLOW, CHECKERED};
+    friend std::ostream &operator<<(std::ostream &os, const Box &var);
     Box();
     Box(int width, int height);
-    Box(int width, int height, bool type);
-    ostream & print(ostream & os) const;
+    Box(int width, int height, Box::BoxType type);
     ~Box();
+    Box(const Box &orig);
     int getWidth() const;
     int getHeight() const;
     int setWidth(int width);
     int setHeight(int height);
-    bool setType(bool type);
     string type() const;
+    Box::BoxType setType(Box::BoxType type);
+    static int howMany() {return _howMany;};
     
 private:
     int _width;
     int _height;
-    bool _type;
+    Box::BoxType _type;
+    static int _howMany;
 };
 
 #endif /* box_h */
