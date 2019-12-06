37d0630b4270f8157b494d783dbd5544db6c302c
jtnashoanak <jtnashoanak@alaska.edu>
2019-12-06 12:31:58 -0900

    initial commit for hw05

---
+++ hw05/hw05/box.cpp
@@ -0,0 +1,6 @@
+//  box.cpp
+//  hw05
+//  Created by Jadon Nashoanak on 12/6/19.
+
+
+#include "box.hpp"
---
+++ hw05/hw05/box.hpp
@@ -0,0 +1,11 @@
+//  box.hpp
+//  hw05
+//  Created by Jadon Nashoanak on 12/6/19.
+
+
+#ifndef box_hpp
+#define box_hpp
+
+
+
+#endif /* box_hpp */
