03a748365758f6384811b569f69deb8bb9456032
jtnashoanak <jtnashoanak@alaska.edu>
2019-12-11 13:06:05 -0900

    made for filled, checkers, and hollow

--- hw05/hw05/box.cpp
+++ hw05/hw05/box.cpp
@@ -2,5 +2,71 @@
 //  hw05
 //  Created by Jadon Nashoanak on 12/6/19.
 
-
 #include "box.hpp"
+#include <memory>
+#include <iostream>
+#include <exception>
+
+std::ostream & operator<<(std::ostream &os, const Box &var) {
+    
+    if(var.type() == "Filled") {
+        
+        for(auto y = 0; y < var.getHeight(); y++) {
+            
+            for(auto x = 0; x < var.getWidth(); x++) {
+                
+                os << "x";
+            }
+            os << std::endl;
+        }
+    }
+    
+    if(var.type() == "Hollow") {
+        
+        for(auto y = 0; y < var.getHeight(); y++) {
+            
+            if(y == 0 || y == (var.getHeight() - 1)) {
+                
+                for(auto x = 0; x < var.getWidth(); x++) {
+                    
+                    os << "x";
+                }
+                os << std::endl;
+            }
+            else {
+                
+                for(auto x = 0; x < var.getWidth(); x++) {
+                    
+                    if(x == 0 || x == (var.getWidth() - 1)) {
+                        
+                        os << "x";
+                    }
+                    else {
+                        
+                        os << ' ';
+                    }
+                }
+                os<<std::endl;
+            }
+        }
+    }
+    
+    if(var.type() == "Checkered") {
+        
+        for(auto y = 0; y < var.getHeight(); y++) {
+            
+            for(auto x = 0; x < var.getWidth(); x++) {
+                
+                int z = ((y + x) % 2);
+                if(z == 0) {
+                    os << "x";
+                }
+                if(z == 1) {
+                    os << ' ';
+                }
+            }
+            os << std::endl;
+        }
+    }
+    return os;
+}
--- hw05/hw05/box.hpp
+++ hw05/hw05/box.hpp
@@ -2,10 +2,32 @@
 //  hw05
 //  Created by Jadon Nashoanak on 12/6/19.
 
-
 #ifndef box_hpp
 #define box_hpp
 
+#include <string>
+using std::string;
+#include <memory>
+#include <ostream>
+using std::ostream;
 
+class Box {
+public:
+    friend ostream & operator<<(ostream &os, const Box &var);
+    Box(int width, int height);
+    Box();
+    virtual ~Box();
+    Box(const Box &orig);
+    int getWidth() const;
+    int getHeight() const;
+    int setWidth(int width);
+    int setHeight(int height);
+    virtual void print(ostream &os) const = 0;
+    virtual string type() const = 0;
+    
+private:
+    int _height;
+    int _width;
+};
 
 #endif /* box_hpp */
